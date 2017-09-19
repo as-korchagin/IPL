3159b5b0893b2335012be47527cacd80c0537421
as-korchagin <as.korchagin@yandex.ru>
2017-09-19 21:13:40 +0300

    reverse_v2, array_average added

--- lab2/arr_algs.py
+++ lab2/arr_algs.py
@@ -5,14 +5,14 @@ def array_min(array):
             min_value = array[i]
     return min_value
 
-	
+
 def array_average(array):
-	sum = 0
-	for i in array:
-		sum += i
-	return sum / len(array)
-	
-	
+    elems_sum = 0
+    for i in array:
+        elems_sum += i
+    return elems_sum / len(array)
+
+
 input_array = list(int(i) for i in input().split(','))
 print(array_min(input_array))
 print(array_average(input_array))
--- lab2/str_algs.py
+++ lab2/str_algs.py
@@ -4,8 +4,9 @@ def reverse(string):
         reversed_string += string[i]
     return reversed_string
 
-	
+
 def reverse_v2(string):
-	return string[::-1]
+    return string[::-1]
+
 
 print(reverse_v2('Hello, World!'))
